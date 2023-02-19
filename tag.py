d10c43d9cc23c72d668b29e71c9e75632ddf5773
xxx595588 <daniel0323.lin@gmail.com>
2023-02-18 16:49:16 -0800

    moved mission to .xml

--- tag.py
+++ tag.py
@@ -7,6 +7,7 @@ import os
 import sys
 import time
 import math
+import json
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -21,45 +22,17 @@ TAGGER_X = -(SIZE-0.5)
 SUR_TIME = []
 FINAL = []
 
-'''plain_map = np.array([
-                     [0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-                     [0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-                     [0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  0],
-                     [0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  1,  0,  0],
-                     [0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1],
-                     [1,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1],
-                     [0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1],
-                     [0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-                     [0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  0,  0,  0],
-                     [0,  0,  0,  0,  1,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  0,  0,  0,  0],
-                     [0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-                     [0,  0,  0,  0,  0,  0,  0,  0,  1,  0,  0,  0,  0,  1,  0,  1,  0,  0,  0,  0],
-                     [0,  0,  0,  1,  0,  0,  0,  0,  1,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-                     [0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  0,  0,  0,  0],
-                     [0,  1,  0,  0,  0,  0,  0,  0,  1,  0,  0,  0,  1,  0,  0,  1,  0,  0,  0,  0],
-                     [0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-                     [0,  0,  0,  0,  1,  0,  1,  0,  0,  0,  0,  0,  0,  0,  1,  0,  0,  0,  0,  0],
-                     [0,  1,  0,  1,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  0,  0,  0],
-                     [0,  0,  0,  0,  0,  0,  0,  0,  1,  0,  1,  0,  0,  0,  0,  0,  0,  0,  1,  0],
-                     [0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  0,  0,  1,  0]])'''
-plain_map = np.array([
-                     [0,  0,  0,  0,  0,  0],
-                     [0,  0,  1,  0,  0,  0],
-                     [0,  0,  0,  0,  0,  0],
-                     [0,  0,  0,  0,  0,  0],
-                     [0,  0,  0,  0,  1,  0],
-                     [0,  1,  0,  0,  0,  0]])
+plain_map = np.array([[0,  0,  0,  0,  0,  0],
+                      [0,  0,  1,  0,  0,  0],
+                      [0,  0,  0,  0,  0,  0],
+                      [0,  0,  0,  0,  0,  0],
+                      [0,  0,  0,  0,  1,  0],
+                      [0,  1,  0,  0,  0,  0]])
 
 my_map = None
 
-if sys.version_info[0] == 2:
-    sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immediately
-else:
-    import functools
-    print = functools.partial(print, flush=True)
-
+# call this function only when need a new map, copy map_txt to plain_map & pg_str into <DrawingDecorator> field in tag.xml 
 def playground(x, y, z, blocktype):
-    global plain_map
     pg_str = ""
 
     # density of obstacles
@@ -86,12 +59,9 @@ def playground(x, y, z, blocktype):
     obs = np.zeros((SIZE **2,), dtype = np.int8)
     obs[np.random.choice(SIZE**2, replace = False, size = int((SIZE**2)*d) )] = 1
     obs = np.reshape(obs, (SIZE ,SIZE))
-    plain_map = np.rot90(obs, 1)
-    print((abs(int(RUNNER_X)), int(SIZE/2-RUNNER_Z+1)))
-    plain_map[abs(int(RUNNER_X))][int(SIZE/2-RUNNER_Z)] = 0
-    plain_map[abs(int(TAGGER_X))][int(SIZE/2-TAGGER_Z)] = 0
-
-    print(plain_map)
+    map_txt = np.rot90(obs, 1)
+    map_txt[abs(int(RUNNER_X))][int(SIZE/2-RUNNER_Z)] = 0
+    map_txt[abs(int(TAGGER_X))][int(SIZE/2-TAGGER_Z)] = 0
   
     x = -(SIZE)
     z = -(int(SIZE/2))
@@ -99,86 +69,9 @@ def playground(x, y, z, blocktype):
     for i, j in zip(np.where(obs == 1)[1], np.where(obs == 1)[0]):
         pg_str += '<DrawLine x1="' + str(x+i) + '" y1="' + str(y) + '" z1="' + str(z+j+1) + '" x2="' + str(x+i) + '" y2="' + str(y+1) + '" z2="' + str(z+j+1) + '" type="bedrock"/>\n'
             
-    return pg_str
+    return map_txt, pg_str
 
-missionXML='''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
-            <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-            
-              <About>
-                <Summary>Tag Game</Summary>
-              </About>
-              
-              <ServerSection>
-                <ServerInitialConditions>
-                  <Time>
-                    <StartTime>1000</StartTime>
-                    <AllowPassageOfTime>false</AllowPassageOfTime>
-                  </Time>
-                  <Weather>clear</Weather>
-                </ServerInitialConditions>
-                <ServerHandlers>
-                  <FlatWorldGenerator generatorString="3;1*minecraft:bedrock,1*minecraft:grass;2;village"/>
-                  <DrawingDecorator>
-                  <DrawLine x1="0" y1="2" z1="0" x2="0" y2="2" z2="4" type="bedrock"/>
-                    <DrawLine x1="0" y1="2" z1="0" x2="0" y2="2" z2="-3" type="bedrock"/>
-                    <DrawLine x1="0" y1="3" z1="0" x2="0" y2="3" z2="4" type="bedrock"/>
-                    <DrawLine x1="0" y1="3" z1="0" x2="0" y2="3" z2="-3" type="bedrock"/>
-                    <DrawLine x1="-7" y1="2" z1="0" x2="-7" y2="2" z2="4" type="bedrock"/>
-                    <DrawLine x1="-7" y1="2" z1="0" x2="-7" y2="2" z2="-3" type="bedrock"/>
-                    <DrawLine x1="-7" y1="3" z1="0" x2="-7" y2="3" z2="4" type="bedrock"/>
-                    <DrawLine x1="-7" y1="3" z1="0" x2="-7" y2="3" z2="-3" type="bedrock"/>
-                    <DrawLine x1="-6" y1="2" z1="4" x2="0" y2="2" z2="4" type="bedrock"/>
-                    <DrawLine x1="-6" y1="2" z1="-3" x2="0" y2="2" z2="-3" type="bedrock"/>
-                    <DrawLine x1="-6" y1="3" z1="4" x2="0" y2="3" z2="4" type="bedrock"/>
-                    <DrawLine x1="-6" y1="3" z1="-3" x2="0" y2="3" z2="-3" type="bedrock"/>
-                    <DrawLine x1="-6" y1="2" z1="-1" x2="-6" y2="3" z2="-1" type="bedrock"/>
-                    <DrawLine x1="-2" y1="2" z1="0" x2="-2" y2="3" z2="0" type="bedrock"/>
-                    <DrawLine x1="-5" y1="2" z1="2" x2="-5" y2="3" z2="2" type="bedrock"/>
-                  </DrawingDecorator>
-                  <ServerQuitFromTimeUp timeLimitMs="1000000"/>
-                  <ServerQuitWhenAnyAgentFinishes/>
-                </ServerHandlers>
-              </ServerSection>
-              
-              <AgentSection mode="Survival">
-                <Name>Runner</Name>
-                <AgentStart>
-                  <Placement x="-0.5" y="2" z="0.5" yaw="90"/>
-                </AgentStart>
-                <AgentHandlers>
-                <DiscreteMovementCommands/>
-                <AbsoluteMovementCommands/>
-                <ObservationFromGrid>
-                <Grid name="floor3x3">
-                <min x="-1" y="0" z="-1"/>
-                <max x="1" y="0" z="1"/>
-                </Grid>
-                </ObservationFromGrid>
-                  <ObservationFromFullStats/>
-                  <ContinuousMovementCommands turnSpeedDegs="180"/>
-                </AgentHandlers>
-              </AgentSection>
-
-              <AgentSection mode="Survival">
-                <Name>Tagger</Name>
-                <AgentStart>
-                  <Placement x="-5.5" y="2" z="0.5" yaw="-90"/>
-                </AgentStart>
-                <AgentHandlers>
-                <DiscreteMovementCommands/>
-                <AbsoluteMovementCommands/>
-                <ObservationFromGrid>
-                <Grid name="floor3x3">
-                <min x="-1" y="0" z="-1"/>
-                <max x="1" y="0" z="1"/>
-                </Grid>
-                </ObservationFromGrid>
-                  <ObservationFromFullStats/>
-                  <ContinuousMovementCommands turnSpeedDegs="180"/>
-                </AgentHandlers>
-              </AgentSection>
-            </Mission>'''
-
+# start the mission safely by catching any exception
 def safeStartMission(agent_host, mission, client_pool, recording, role, experimentId):
     used_attempts = 0
     max_attempts = 5
@@ -213,6 +106,7 @@ def safeStartMission(agent_host, mission, client_pool, recording, role, experime
             exit(1)
     print("startMission called okay.")
 
+# wait all agent start their mission
 def safeWaitForStart(agent_hosts):
     print("Waiting for the mission to start", end=' ')
     start_flags = [False for a in agent_hosts]
@@ -236,6 +130,7 @@ def safeWaitForStart(agent_hosts):
         exit(1)
     print("Mission has started.")
 
+# control the movement of tagger
 def tagger_movement(tagger, tagger_action, tagger_cur_pos):
     global TAGGER_Z, TAGGER_X
 
@@ -252,20 +147,24 @@ def tagger_movement(tagger, tagger_action, tagger_cur_pos):
         tagger.sendCommand("movewest")
         TAGGER_X -= 1
 
+# control the movement of runner
 def runner_movement(runner, A, runner_cur_pos, tagger_cur_pos):
     global RUNNER_Z, RUNNER_X, TAGGER_Z, TAGGER_X
     
     """
-          W
-          |
-        6 3 0
-    S-- 7 4 1 --N
-        8 5 2
-          |
-          E
     position 4 is runner's current position
+
+               W (-x)
+               |
+             6 3 0
+    S (+z)-- 7 4 1 --N (-z)
+             8 5 2
+               |
+               E (+x)
+    
     """
-    ori_dis = math.sqrt((RUNNER_Z-TAGGER_Z)**2 + (RUNNER_X-TAGGER_X)**2)
+    
+    ori_dis = math.sqrt((runner_cur_pos[0]-tagger_cur_pos[0])**2 + (runner_cur_pos[1]-tagger_cur_pos[1])**2)
 
     if A == "movenorth":
         if SIZE-1-runner_cur_pos[0]-1 >= 0 and plain_map[runner_cur_pos[1]][SIZE-1-runner_cur_pos[0]-1] != 1:
@@ -296,7 +195,7 @@ def runner_movement(runner, A, runner_cur_pos, tagger_cur_pos):
         else:
             next_S = (runner_cur_pos, tagger_cur_pos)
 
-    new_dis = math.sqrt((RUNNER_Z-TAGGER_Z)**2 + (RUNNER_X-TAGGER_X)**2)
+    new_dis = math.sqrt((runner_cur_pos[0]-tagger_cur_pos[0])**2 + (runner_cur_pos[1]-tagger_cur_pos[1])**2)
 
     if new_dis <= math.sqrt(2):
         reward = -100
@@ -315,94 +214,93 @@ def gameover(runner_cur_pos, tagger_cur_pos):
     return False
     
 def main():
-  # Create default Malmo objects:
-  runner = MalmoPython.AgentHost()
-  tagger = MalmoPython.AgentHost()
-  my_mission = MalmoPython.MissionSpec(missionXML, True)
-  runner_mission_record = MalmoPython.MissionRecordSpec()
-  tagger_mission_record = MalmoPython.MissionRecordSpec()
+    global RUNNER_Z, RUNNER_X, TAGGER_Z, TAGGER_X, ACTIONS, SIZE, ITERATION, plain_map, my_map, SUR_TIME, FINAL
 
-  client_pool = MalmoPython.ClientPool()
-  client_pool.add( MalmoPython.ClientInfo("127.0.0.1",10000) )
-  client_pool.add( MalmoPython.ClientInfo("127.0.0.1",10001) )
+    runner_coor = (int(SIZE/2-RUNNER_Z+1), abs(int(RUNNER_X)))
+    tagger_coor = (int(SIZE/2-TAGGER_Z+1), abs(int(TAGGER_X)))
 
-  safeStartMission(runner, my_mission, client_pool, runner_mission_record, 0, "")
-  safeStartMission(tagger, my_mission, client_pool, tagger_mission_record, 1, "")
-  safeWaitForStart([runner, tagger])
+    pmap = playgroundMap(plain_map, runner_coor[0], runner_coor[1], tagger_coor[0], tagger_coor[1])
+    my_map = map(plain_map)
+    agent = QL_agent(ACTIONS, len(plain_map[0]), len(plain_map))
+    copy_map = plain_map
 
-  print("Mission running ", end=' ')
+    mission_file = "./tag.xml"
+    with open(mission_file, "r") as f:
+        print(f"Loading mission from {mission_file}")
+        mission_xml = f.read()
+        my_mission = MalmoPython.MissionSpec(mission_xml, True)
 
-  global RUNNER_Z, RUNNER_X, TAGGER_Z, TAGGER_X, ACTIONS, SIZE, ITERATION, plain_map, SUR_TIME
+    runner = MalmoPython.AgentHost()
+    tagger = MalmoPython.AgentHost()
 
-  pmap = playgroundMap(plain_map, int(SIZE/2-RUNNER_Z+1), abs(int(RUNNER_X)), int(SIZE/2-TAGGER_Z+1), abs(int(TAGGER_X)))
-  my_map = map(plain_map)
-  copy_map = plain_map
-  agent = QL_agent(ACTIONS, SIZE, SIZE)
+    runner_mission_record = MalmoPython.MissionRecordSpec()
+    tagger_mission_record = MalmoPython.MissionRecordSpec()
 
-  for i in range(ITERATION):
-    start = time.time()
-    print(f"Iteration {i+1}")
-    plain_map = copy_map
-    RUNNER_Z = 0.5
-    RUNNER_X = -0.5
-    TAGGER_Z = 0.5
-    TAGGER_X = -(SIZE-0.5)
-    terminated = False
-    my_map.update_start((int(SIZE/2-TAGGER_Z+1), abs(int(TAGGER_X))))
-    my_map.update_end((int(SIZE/2-RUNNER_Z+1), int(abs(RUNNER_X))))
+    client_pool = MalmoPython.ClientPool()
+    client_pool.add( MalmoPython.ClientInfo("127.0.0.1",10000) )
+    client_pool.add( MalmoPython.ClientInfo("127.0.0.1",10001) )
 
-    runner.sendCommand("tp -0.5 2 0.5")
-    tagger.sendCommand("tp -5.5 2 0.5")
-    #time.sleep(1)
-    while True:
-        S =((int(SIZE/2-RUNNER_Z+1), abs(int(RUNNER_X))), (int(SIZE/2-TAGGER_Z+1), abs(int(TAGGER_X))))
-        #print("\n\n")
-        #print(".", end="")
-        #print(f"tagger at {int(SIZE/2-TAGGER_Z+1), abs(int(TAGGER_X))}")
-        #print(f"runner at {int(SIZE/2-RUNNER_Z+1), abs(int(RUNNER_X))}")
-        runner_world_state = runner.getWorldState()
-        tagger_world_state = tagger.getWorldState()
+    safeStartMission(runner, my_mission, client_pool, runner_mission_record, 0, "")
+    safeStartMission(tagger, my_mission, client_pool, tagger_mission_record, 1, "")
+    safeWaitForStart([runner, tagger])
 
-        my_map.reset()
-        my_map.find_shortest_path()
+    print("Mission running ", end=' ')
 
-        if gameover((int(SIZE/2-RUNNER_Z+1), abs(int(RUNNER_X))), (int(SIZE/2-TAGGER_Z+1), abs(int(TAGGER_X)))):
-            end = time.time()
-            terminated = True
-        else:
-            tagger_action = my_map.retrieve()[-1]
+    for i in range(ITERATION):
+        print(f"Iteration {i+1}")
+        start = time.time()
+        plain_map = copy_map
+        terminated = False
 
-        my_map.update_start(tagger_action)
-        tagger_movement(tagger, tagger_action, (int(SIZE/2-TAGGER_Z+1), abs(int(TAGGER_X))))
-        A = agent.choose_action(S)
-        next_S, R = runner_movement(runner, A, (int(SIZE/2-RUNNER_Z+1), int(abs(RUNNER_X))), (int(SIZE/2-TAGGER_Z+1), int(abs(TAGGER_X))))
-        agent.update_qtable(A, S, next_S, R, terminated)
-        pmap.render(int(SIZE/2-RUNNER_Z+1), abs(int(RUNNER_X)), 0)
-        pmap.render(int(SIZE/2-TAGGER_Z+1), abs(int(TAGGER_X)), 1)
+        RUNNER_Z = 0.5
+        RUNNER_X = -0.5
+        TAGGER_Z = 0.5
+        TAGGER_X = -(SIZE-0.5)
+
+        my_map.update_start((int(SIZE/2-TAGGER_Z+1), abs(int(TAGGER_X))))
         my_map.update_end((int(SIZE/2-RUNNER_Z+1), int(abs(RUNNER_X))))
 
-        for error in runner_world_state.errors:
-            print("Error:",error.text)
-        for error in tagger_world_state.errors:
-            print("Error:",error.text)
-        #time.sleep(1)
+        runner.sendCommand("tp -0.5 2 0.5")
+        tagger.sendCommand("tp -5.5 2 0.5")
+        while True:
+            S =((int(SIZE/2-RUNNER_Z+1), abs(int(RUNNER_X))), (int(SIZE/2-TAGGER_Z+1), abs(int(TAGGER_X))))
+            #print("\n\n")
+            #print(".", end="")
+            #print(f"tagger at {int(SIZE/2-TAGGER_Z+1), abs(int(TAGGER_X))}")
+            #print(f"runner at {int(SIZE/2-RUNNER_Z+1), abs(int(RUNNER_X))}")
 
-        if terminated:
-            break
+            my_map.reset()
+            my_map.find_shortest_path()
 
-    SUR_TIME.append(end-start)
+            if gameover((int(SIZE/2-RUNNER_Z+1), abs(int(RUNNER_X))), (int(SIZE/2-TAGGER_Z+1), abs(int(TAGGER_X)))):
+                end = time.time()
+                terminated = True
+            else:
+                tagger_action = my_map.retrieve()[-1]
 
-    if len(SUR_TIME) == 50:
-        med = np.average(SUR_TIME)
-        FINAL.append(med)
-        SUR_TIME = []
-    print("Game over!")
+            my_map.update_start(tagger_action)
+            tagger_movement(tagger, tagger_action, (int(SIZE/2-TAGGER_Z+1), abs(int(TAGGER_X))))
+            A = agent.choose_action(S)
+            next_S, R = runner_movement(runner, A, (int(SIZE/2-RUNNER_Z+1), int(abs(RUNNER_X))), (int(SIZE/2-TAGGER_Z+1), int(abs(TAGGER_X))))
+            agent.update_qtable(A, S, next_S, R, terminated)
+            pmap.render(int(SIZE/2-RUNNER_Z+1), abs(int(RUNNER_X)), 0)
+            pmap.render(int(SIZE/2-TAGGER_Z+1), abs(int(TAGGER_X)), 1)
+            my_map.update_end((int(SIZE/2-RUNNER_Z+1), int(abs(RUNNER_X))))
 
-  plt.plot(range(len(FINAL)), FINAL)
-  plt.show()
-  agent.export_data()
-  print(FINAL)
-    
+            if terminated:
+                break
+
+        SUR_TIME.append(end-start)
+
+        if len(SUR_TIME) == 50:
+            med = np.average(SUR_TIME)
+            FINAL.append(med)
+            SUR_TIME = []
+        print("Game over!")
+
+    plt.plot(range(len(FINAL)), FINAL)
+    plt.show()
+    agent.export_data()   
     # Mission has ended.
 
 if __name__ == "__main__":
